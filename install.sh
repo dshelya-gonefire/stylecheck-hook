3ac1b1d7bd3494b7e7de2276aeb6681d2b5c0e06
Dmitry Shelya <dmitry.shelya@gmail.com>
2017-08-09 00:24:48 +0300

    Add handling of repositories having spaces in its file-path

--- install.sh
+++ install.sh
@@ -14,28 +14,28 @@ ENGINE_DIR=~/stylecheck
 GIT_HOOKS_DIR=$1/.git/hooks
 
 if [ ! -d "$GIT_HOOKS_DIR" ]; then
-    echo "Invalid repository path"
+    echo "Invalid repository path: [$GIT_HOOKS_DIR]"
 	exit
 fi
 
 echo "Installing hooks into repository: $GIT_HOOKS_DIR"
-mkdir -p $ENGINE_DIR
-cp checker/* $ENGINE_DIR
+mkdir -p "$ENGINE_DIR"
+cp checker/* "$ENGINE_DIR"
  
 git config --global --unset-all check.engine
 git config --global --unset-all check.style
-git config --global --add check.engine $ENGINE_DIR/checkstyle.jar
-git config --global --add check.style $ENGINE_DIR/style.xml
+git config --global --add check.engine "$ENGINE_DIR/checkstyle.jar"
+git config --global --add check.style "$ENGINE_DIR/style.xml"
 git config --global alias.verify '!sh .git/hooks/pre-commit'
  
-cp hook/* $GIT_HOOKS_DIR
+cp hook/* "$GIT_HOOKS_DIR"
 
 ### Create aliased installer script for patched repository
 if [[ "$1" != "." ]]; then
-    ALIAS_NAME=`echo $1 | sed 's/.*\/\([a-zA-Z0-9_.-]\+$\)/\1/gi'`
+    ALIAS_NAME=`echo $1 | sed 's/.*\/\([a-zA-Z0-9_. -]\+$\)/\1/gi'`
 	else
-	ALIAS_NAME=`pwd | sed 's/.*\/\([a-zA-Z0-9_.-]\+$\)/\1/gi'`
+	ALIAS_NAME=`pwd | sed 's/.*\/\([a-zA-Z0-9_. -]\+$\)/\1/gi'`
 fi
 
-echo "./install.sh $1" > install.$ALIAS_NAME.sh
-
+echo "./install.sh $1" > "install.$ALIAS_NAME.sh"
+echo Done!
